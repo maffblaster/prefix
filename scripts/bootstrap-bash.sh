1b5455970ef527a642ad2b571484b49f8e3c08d4
Matthew Marchese <maffblaster@gentoo.org>
2022-05-25 19:12:43 -0700

    bootstrap-bash.sh: Revise/simplify script logic.
    Add control variables and improve output.
    
    Signed-off-by: Matthew Marchese <maffblaster@gentoo.org>

--- scripts/bootstrap-bash.sh
+++ scripts/bootstrap-bash.sh
@@ -3,22 +3,30 @@
 
 # bash installer
 #
-#  POSIX (?) /bin/sh which doesn't eat most of the stuff in the
+# POSIX (?) /bin/sh which doesn't eat most of the stuff in the
 # bootstrap-prefix script, among which the most important part:
 # dynamic function calling.  So, we need to bootstrap bash outside the
 # bootstrap script, which is the purpose of this script.
+
+# TODO: `mktemp -d` can be used to safely create a random temp directory...
+
+PREFIX_BOOTSTRAP_BASH_VERSION="bash-5.1" # Upstream supports bash-5.1.xxx for patch set.
+PREFIX_BOOTSTRAP_BASH_BUILD_DIRECTORY="" # Declare variable for later use.
+
+# The following URI can be used to download bash directly from upstream...
+#GNU_BASH_URI="https://ftp.gnu.org/gnu/bash/${PREFIX_BOOTSTRAP_BASH_VERSION}.tar.gz"
+
+GENTOO_MIRRORS=${GENTOO_MIRRORS:="http://distfiles.gentoo.org/distfiles"}
 
 if [ -z "${1}" ] ; then
 	echo "usage: ${0} <location>" > /dev/stderr
 	exit 255
 fi
 
-mkdir -p "${1}"
-cd "${1}" || exit 1
-mkdir bash-build
-cd bash-build || exit 1
+PREFIX_BOOTSTRAP_BASH_BUILD_DIRECTORY="${1}"
 
-GENTOO_MIRRORS=${GENTOO_MIRRORS:="http://distfiles.gentoo.org/distfiles"}
+mkdir -p "${PREFIX_BOOTSTRAP_BASH_BUILD_DIRECTORY}/bash-build" || exit 1
+cd "${PREFIX_BOOTSTRAP_BASH_BUILD_DIRECTORY}" || exit 1
 
 command_exists() {
 	check_cmd="${1}"
@@ -36,12 +44,12 @@ same_file() {
 	fi
 }
 
-if [ ! -e bash-4.2.tar.gz ] ; then
-	eerror() { echo "!!! ${*}" 1>&2; }
-	einfo() { echo "* ${*}"; }
+if [ ! -e "${PREFIX_BOOTSTRAP_BASH_VERSION}.tar.gz" ] ; then
+	eerror() { echo -e "!!! ${*}" 1>&2; }
+	einfo() { echo -e "* ${*}"; }
 
 	if [ -z "${FETCH_COMMAND}" ] ; then
-		# Try to find a download manager, this script only deal with wget,
+		# Try to find a download manager, this script only deals with wget,
 		# curl, FreeBSD's fetch, and ftp...
 		if command_exists wget; then
 			FETCH_COMMAND="wget"
@@ -66,19 +74,32 @@ if [ ! -e bash-4.2.tar.gz ] ; then
 			esac
 		fi
 		if [ -z "${FETCH_COMMAND}" ]; then
-			eerror "no suitable download manager found (need wget, curl, fetch or ftp)"
-			eerror "could not download ${1##*/}"
-			eerror "download the file manually, and put it in ${PWD}"
+			eerror "No suitable download manager found (need wget, curl, fetch or ftp)."
+			eerror "Could not download ${1##*/}"
+			eerror "Download the file manually, and put it in ${PWD}"
 			exit 1
 		fi
 	fi
-	${FETCH_COMMAND} "${GENTOO_MIRRORS}/bash-4.2.tar.gz" < /dev/null
+	${FETCH_COMMAND} "${GENTOO_MIRRORS}/${PREFIX_BOOTSTRAP_BASH_VERSION}.tar.gz" < /dev/null
 fi
 
-gzip -d bash-4.2.tar.gz
-tar -xf bash-4.2.tar
-cd bash-4.2 || exit 1
+gzip -d ${PREFIX_BOOTSTRAP_BASH_VERSION}.tar.gz
+tar -xf ${PREFIX_BOOTSTRAP_BASH_VERSION}.tar
+cd ${PREFIX_BOOTSTRAP_BASH_VERSION} || exit 1
 
-./configure --prefix="${1}"/usr --disable-nls
+./configure --prefix="${PREFIX_BOOTSTRAP_BASH_BUILD_DIRECTORY}/usr" --disable-nls
 make
 make install
+
+if [ "${?}" -eq "0" ]; then
+	echo
+	einfo "${PREFIX_BOOTSTRAP_BASH_VERSION} compiled successfully. Run:"
+	einfo "  export PATH='/var/tmp/bash/usr/bin:\${PATH}'"
+	einfo "To add ${PREFIX_BOOTSTRAP_BASH_VERSION} to the shell's PATH variable."
+	echo
+else
+	echo
+	einfo "${PREFIX_BOOTSTRAP_BASH_VERSION} compilation failed."
+	einfo "It may be necessary to file a bug."
+	echo
+fi
